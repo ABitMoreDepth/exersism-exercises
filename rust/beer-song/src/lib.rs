07b3f537b9c72107e8ba7ef485ff1f91f4914ede
Sam Martin <sam@abitmoredepth.com>
2019-09-26 22:57:22 +0100

    Tweak to use a format string for the majority of cases.

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,35 +1,31 @@
 pub fn verse(n: i32) -> String {
     let mut output = String::new();
-    let n_str: String = n.to_string();
 
     match n {
         0 => {
-            output.push_str("No more bottles of beer on the wall, no more bottles of beer.\n");
-            output.push_str("Go to the store and buy some more, 99 bottles of beer on the wall.\n");
-        }
-        1 => {
-            output = "1 bottle of beer on the wall, 1 bottle of beer.\n".to_owned()
-                + &"Take it down and pass it around, no more bottles of beer on the wall.\n"
-                    .to_string();
+            output.clear();
+            output.push_str(concat!(
+                "No more bottles of beer on the wall, no more bottles of beer.\n",
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n",
+            ));
         }
         _ => {
-            output.push_str(&n_str);
-            if n == 1 {
-                output.push_str(" bottle of beer on the wall, ");
-                output.push_str(&n_str);
-                output.push_str(" bottle of beer.\nTake one down and pass it around, ");
-            } else {
-                output.push_str(" bottles of beer on the wall, ");
-                output.push_str(&n_str);
-                output.push_str(" bottles of beer.\nTake one down and pass it around, ");
-            }
-            if n == 2 {
-                output.push_str(&(n - 1).to_string());
-                output.push_str(" bottle of beer on the wall.\n");
-            } else {
-                output.push_str(&(n - 1).to_string());
-                output.push_str(" bottles of beer on the wall.\n");
-            }
+            output.push_str(&format!(
+                concat!(
+                    "{num} bottle{plural} of beer on the wall, {num} bottle{plural} ",
+                    "of beer.\nTake {singular} down and pass it around, {remaining} ",
+                    "bottle{rem_plural} of beer on the wall.\n",
+                ),
+                num = n,
+                plural = if n > 1 { "s" } else { "" },
+                singular = if n == 1 { "it" } else { "one" },
+                remaining = if n > 1 {
+                    (n - 1).to_string()
+                } else {
+                    "no more".to_string()
+                },
+                rem_plural = if n > 2 || n == 1 { "s" } else { "" },
+            ));
         }
     }
 
@@ -40,11 +36,12 @@ pub fn sing(start: i32, end: i32) -> String {
     let mut counter: i32 = start;
     let mut output = String::new();
 
-    println!("Start: {}, End: {}", start.to_string(), end.to_string());
-    // for counter in (start..end).rev() {
     while counter >= end {
-        println!("Creating verse for line {}", counter.to_string());
-        output.push_str(&verse(counter));
+        output.push_str(&format!(
+            "{}{}",
+            &verse(counter),
+            if counter - 1 >= end { "\n" } else { "" }
+        ));
         counter -= 1;
     }
     output
